8ff4e7192b2d61fd214ed291ef832df639305a44
Konstantin Mikhliuk <mikhliuk.k@gmail.com>
2019-01-28 00:53:47 +0300

    checkpoint

--- predictor.py
+++ predictor.py
@@ -17,19 +17,18 @@ if match[1] and match[2]:
     out_file_path = f'{match[1]}.out.{match[2]}'
 else:
     out_file_path = f'{file_path}.out'
-review_count = args.reviews_count
+review_count = int(args.reviews_count)
 
 print('Opening file...')
 data = pd.read_csv(file_path)
-data = data[len(data) - 5000:len(data) - 1]
 
 print('Preparing data...')
 # Filter bad values
 data = data[data['review'].apply(lambda x: isinstance(x, str))]
 data = data[data['stars'].apply(lambda x: not np.isnan(x))]
 if review_count:
-    data = data[0:int(args.reviews_count)]
-data['review'] = data['review'].apply(lambda x: ''.join(re.findall('\w+', x)))
+    data = data[0:review_count]
+data['review_clear'] = data['review'].apply(lambda x: ''.join(re.findall('\w+', x)))
 data['created_at'] = data['created_at'].apply(pd.to_datetime)
 data['year'] = data['created_at'].apply(lambda x: x.year)
 data['normalized_stars'] = data['stars'].apply(lambda x: (x - 1) / 4)
@@ -54,7 +53,7 @@ for idx, val in enumerate(sorted_words):
     index2word[idx] = val[0]
     word2index[val[0]] = idx
 
-data['review_indexes'] = data['review'].apply(lambda x: list(map(lambda word: word2index[word], list(x))))
+data['review_indexes'] = data['review_clear'].apply(lambda x: list(map(lambda word: word2index[word], list(x))))
 
 print('Loading model...')
 model = load_model('models/jd_reviews_model.h5')
@@ -70,6 +69,7 @@ for i in range(0, len(review_indexes_padded), 1000):
 print('Saving results...')
 result = pd.DataFrame(data={'review_id': [], 'stars': [], 'predicted_stars': []})
 result['review_id'] = data['review_id']
+result['review'] = data['review']
 result['stars'] = data['stars'].apply(int)
 result['predicted_stars'] = predicted_values.apply(lambda x: round(x * 4 + 1)).to_list()
 result.to_csv(out_file_path, index=False, header=True)
